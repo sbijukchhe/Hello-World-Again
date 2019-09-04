2d226da611e7750aa6bb82fc8d02b549395758fd
GBTC441013UR <GBTC441013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-04 14:47:19 -0400

    Hello world

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println ("Hello, World!");
+        System.out.println("Today is September 04. and I am alive");
+    }
+}
